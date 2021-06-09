f8ca805f21d2bbd27ac0c566770b5520fc079d5d
ddomi2048 <ddomi2048@gmail.com>
2021-06-09 19:36:36 +0200

    Angular webshop-app beta

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './_components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
